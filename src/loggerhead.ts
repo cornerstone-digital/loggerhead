28c8e1d62be43e4c6cc5c575c048358264b7ac26
Martin Egan <martingegan@gmail.com>
2019-10-17 13:01:48 +0100

    Added config interface

--- src/loggerhead.ts
+++ src/loggerhead.ts
@@ -1,4 +1,11 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
 // import "core-js/fn/array.find"
 // ...
-export default class Loggerhead {}
+interface LoggerheadConfig {}
+
+export default class Loggerhead {
+  private _config: LoggerheadConfig
+  constructor(config: LoggerheadConfig) {
+    this._config = config
+  }
+}
