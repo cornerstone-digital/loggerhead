6d07f1725a75772561106e9b59860e7b31b8b309
Borja Miralles <borja.gascon@vodafone.com>
2019-10-17 14:12:45 +0100

    wip

--- src/loggerhead.ts
+++ src/loggerhead.ts
@@ -1,11 +1,95 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
 // import "core-js/fn/array.find"
 // ...
-interface LoggerheadConfig {}
+
+import debug, { IDebugger } from 'debug'
+import dayjs from 'dayjs'
+import dayjsPluginUTC from 'dayjs-plugin-utc'
+
+export interface LoggerheadConfig {
+  namespace: string
+  enabled: boolean
+  level: LogLevels
+}
+
+export enum LogLevels {
+  OFF = 0,
+  FATAL = 1,
+  ERROR = 2,
+  WARN = 3,
+  INFO = 4,
+  DEBUG = 5,
+  TRACE = 6,
+  ALL = 7
+}
 
 export default class Loggerhead {
   private _config: LoggerheadConfig
+  public instance: IDebugger
+  private level: LogLevels
   constructor(config: LoggerheadConfig) {
     this._config = config
+    this.instance = debug(config.namespace)
+    this.instance.enabled = config.enabled
+    this.level = config.level
+
+    return this
+  }
+
+  public loggingEnabled() {
+    return this._config.enabled
+  }
+
+  public trace(...args: any | any[]): void {
+    if (this.loggingEnabled() && this.level >= LogLevels.TRACE) {
+      args.unshift(LogLevels[LogLevels.INFO])
+      // args.unshift(this.getTimestamp(timestampFormat))
+      this.instance.apply(this, args)
+    }
+  }
+
+  public info(...args: any | any[]): void {
+    if (this.loggingEnabled() && this.level >= LogLevels.INFO) {
+      args.unshift(LogLevels[LogLevels.INFO])
+      // args.unshift(this.getTimestamp(timestampFormat))
+      this.instance.apply(this, args)
+    }
+  }
+
+  public debug(...args: any | any[]): void {
+    if (this.loggingEnabled() && this.level >= LogLevels.DEBUG) {
+      args.unshift(LogLevels[LogLevels.INFO])
+      // args.unshift(this.getTimestamp(timestampFormat))
+      this.instance.apply(this, args)
+    }
+  }
+
+  public warn(...args: any | any[]): void {
+    if (this.loggingEnabled() && this.level >= LogLevels.WARN) {
+      args.unshift(LogLevels[LogLevels.INFO])
+      // args.unshift(this.getTimestamp(timestampFormat))
+      this.instance.apply(this, args)
+    }
+  }
+
+  public error(...args: any | any[]): void {
+    if (this.loggingEnabled() && this.level >= LogLevels.ERROR) {
+      args.unshift(LogLevels[LogLevels.INFO])
+      // args.unshift(this.getTimestamp(timestampFormat))
+      this.instance.apply(this, args)
+    }
+  }
+
+  public fatal(...args: any | any[]): void {
+    if (this.loggingEnabled() && this.level >= LogLevels.FATAL) {
+      args.unshift(LogLevels[LogLevels.INFO])
+      // args.unshift(this.getTimestamp(timestampFormat))
+      this.instance.apply(this, args)
+    }
+  }
+
+  private getTimestamp(format: string) {
+    dayjs.extend(dayjsPluginUTC)
+    return dayjs.utc().format(format)
   }
 }
--- test/loggerhead.test.ts
+++ test/loggerhead.test.ts
@@ -1,14 +1,123 @@
-import Loggerhead from '../src/loggerhead'
+import Loggerhead, { LoggerheadConfig, LogLevels } from '../src/loggerhead'
+
+const defaultConfig: LoggerheadConfig = {
+  namespace: 'test',
+  enabled: true,
+  level: 0
+}
+
+const callDebugLevels = (logger: Loggerhead) => {
+  logger.fatal('FATAL')
+  logger.error('ERROR')
+  logger.warn('WARN')
+  logger.info('INFO')
+  logger.debug('DEBUG')
+  logger.trace('TRACE')
+}
+
+const createLogger = (level: LogLevels) => {
+  const loggerConfig: LoggerheadConfig = {
+    ...defaultConfig,
+    level
+  }
+
+  return new Loggerhead(loggerConfig)
+}
 
 /**
  * Dummy test
  */
-describe('Dummy test', () => {
+describe('Functionallity test', () => {
   it('works if true is truthy', () => {
     expect(true).toBeTruthy()
   })
 
   it('DummyClass is instantiable', () => {
-    expect(new Loggerhead()).toBeInstanceOf(Loggerhead)
+    const loggerConfig = {
+      ...defaultConfig,
+      level: 1
+    }
+
+    expect(new Loggerhead(loggerConfig)).toBeInstanceOf(Loggerhead)
+  })
+})
+
+describe('Loggerhead', () => {
+  describe('logginEnabled()', () => {
+    it('Should be enabled by default', () => {
+      const LoggerheadConfig = { ...defaultConfig }
+      const instance = new Loggerhead(LoggerheadConfig)
+
+      expect(instance.loggingEnabled()).toBe(true)
+    })
+
+    it('Should return false if disabled', () => {
+      const LoggerheadConfig = { ...defaultConfig, enabled: false }
+      const instance = new Loggerhead(LoggerheadConfig)
+
+      expect(instance.loggingEnabled()).toBe(false)
+    })
+  })
+
+  describe('OFF mode', () => {
+    it('should not call debugger for any level', () => {
+      const logger = createLogger(LogLevels.OFF)
+      const spy = jest.spyOn(logger, 'instance')
+
+      callDebugLevels(logger)
+      expect(spy).toBeCalledTimes(0)
+    })
+  })
+
+  describe('logLevels', () => {
+    it('Fatal', () => {
+      const logger = createLogger(LogLevels.FATAL)
+      const spy = jest.spyOn(logger, 'instance')
+
+      callDebugLevels(logger)
+      expect(spy).toBeCalledTimes(1)
+    })
+    it('Error', () => {
+      const logger = createLogger(LogLevels.ERROR)
+      const spy = jest.spyOn(logger, 'instance')
+
+      callDebugLevels(logger)
+      expect(spy).toBeCalledTimes(2)
+    })
+    it('Warn', () => {
+      const logger = createLogger(LogLevels.WARN)
+      const spy = jest.spyOn(logger, 'instance')
+
+      callDebugLevels(logger)
+      expect(spy).toBeCalledTimes(3)
+    })
+    it('Info', () => {
+      const logger = createLogger(LogLevels.INFO)
+      const spy = jest.spyOn(logger, 'instance')
+
+      callDebugLevels(logger)
+      expect(spy).toBeCalledTimes(4)
+    })
+    it('Debug', () => {
+      const logger = createLogger(LogLevels.DEBUG)
+      const spy = jest.spyOn(logger, 'instance')
+
+      callDebugLevels(logger)
+      expect(spy).toBeCalledTimes(5)
+    })
+    it('Trace', () => {
+      const logger = createLogger(LogLevels.TRACE)
+      const spy = jest.spyOn(logger, 'instance')
+
+      callDebugLevels(logger)
+      expect(spy).toBeCalledTimes(6)
+    })
+    it('All', () => {
+      const logger = createLogger(LogLevels.ALL)
+      const spy = jest.spyOn(logger, 'instance')
+
+      callDebugLevels(logger)
+      expect(spy).toBeCalledTimes(6)
+    })
   })
 })
