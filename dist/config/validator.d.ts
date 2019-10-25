29063ff8f5439f59c47c075dc566e5ff9ba3433c
Martin Egan <martingegan@gmail.com>
2019-10-25 07:42:26 +0100

    Removed dist and coverage

--- dist/config/validator.d.ts
+++
@@ -1,4 +0,0 @@
-import { LoggerheadConfig } from '../types/index.types';
-declare const getConfig: (config: LoggerheadConfig) => LoggerheadConfig;
-export default getConfig;
-//# sourceMappingURL=validator.d.ts.map
